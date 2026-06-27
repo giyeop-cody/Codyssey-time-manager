567af39e881521c4640e5d1edeed40e10262b541
giyeop-cody <cody.giyeop@gmail.com>
2026-06-27 13:02:31 +0000

    fix import

--- app/src/main/java/kr/codyssey/campus/access/ui/DashboardScreen.kt
+++ app/src/main/java/kr/codyssey/campus/access/ui/DashboardScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.webkit.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
